9430ade9a5dbe557e66513f4addda9a532e0492a
Axaeroq <macabrovescaleta@gmail.com>
2026-05-29 22:05:54 -0600

    Upgrade2

--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // Iniciamos la sesión para poder destruirla
-session_unset(); // Borramos todas las variables de sesión
-session_destroy(); // Destruimos la sesión completamente
-header("Location: Tienda.php"); // Redirigimos al usuario a la tienda
-exit(); // Terminamos la ejecución
-?>
